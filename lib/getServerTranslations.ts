c6e9b1fd81dfe64817c311d23f7026099d719f01
Tomilayo21 <cusceda@yahoo.com>
2026-05-25 15:31:11 +0100

    First Commit

--- lib/getServerTranslations.ts
+++
@@ -1,5 +0,0 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
-export async function getServerTranslations(locale: string, namespaces: string[] = ['common']) {
-  return await serverSideTranslations(locale, namespaces);
-}
