3df668d9adf47cb40cc919d246977401b8a8b40c
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2026-06-01 09:07:57 +0100

    page.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <FeaturedProduct /> */}
+      <FeaturedProduct /> 
       {/* <Products /> */}
       {/* <AddProductPanel />
       <ProductListPanel /> */}
